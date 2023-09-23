c1b4390fc046c58fd1922a0cdc1f46f4f5e0d9de
Jon <jonathanyashayev11@gmail.com>
2023-09-23 17:07:24 -0400

    Temp Update Nav

--- frontend/components/shared/navbar.tsx
+++ frontend/components/shared/navbar.tsx
@@ -11,7 +11,7 @@ export default function NavBar() {
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <nav className="w-full">
+    <nav className="bg-stone-100 w-full">
       <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
         <div>
           <div className="flex items-center justify-between py-3">
@@ -60,9 +60,8 @@ export default function NavBar() {
         </div>
         <div>
           <div
-            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-              navbar ? 'block' : 'hidden'
-            }`}>
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? 'block' : 'hidden'
+              }`}>
             <ul className="items-center  justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
               <li className="text-gray-500 text-xl font-bold hover:text-primary-black">
                 <Link href="/">Home</Link>
