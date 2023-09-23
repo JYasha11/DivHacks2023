958b1dd7c363038f7616ba45a4f85b4cb61be6ee
shvam0000 <shvam0000@gmail.com>
2023-09-23 17:55:38 -0400

    🎨 update contact-form

--- frontend/components/contact/contact-form.tsx
+++ frontend/components/contact/contact-form.tsx
@@ -1,64 +1,78 @@
 import React from 'react';
+import Image from 'next/image';
 import { Button } from '../shared';
+
+import Contact from '@/utils/images/contact.png';
 
 const ContactForm = () => {
   return (
-    <section className="bg-white">
-      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 ">
-          Contact Us
-        </h2>
-        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500  sm:text-xl">
-          Got a medical issue and dont know what do do about it? Need details
-          about your next consultation? Let us know.
-        </p>
-        <form action="#" className="space-y-8">
-          <div>
-            <label
-              htmlFor="email"
-              className="block mb-2 text-sm font-medium text-gray-900">
-              Your email
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 "
-              placeholder="johndoe@gmail.com"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="subject"
-              className="block mb-2 text-sm font-medium text-gray-900 ">
-              Subject
-            </label>
-            <input
-              type="text"
-              id="subject"
-              className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 "
-              placeholder="Let us know how we can help you"
-              required
-            />
-          </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="message"
-              className="block mb-2 text-sm font-medium text-gray-900 ">
-              Your message
-            </label>
-            <textarea
-              id="message"
-              rows={6}
-              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
-              placeholder="Leave a comment..."></textarea>
-          </div>
-          <Button type="primary">
-            <span>Send message</span>
-          </Button>
-        </form>
-      </div>
-    </section>
+    <div className="flex justify-center items-center">
+      <figure className="max-w-50">
+        <Image
+          src={Contact}
+          height={700}
+          width={500}
+          alt="Healthcare support"
+          className="rounded-xl"
+        />
+      </figure>
+      <section className="bg-white max-w-50">
+        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 ">
+            Contact Us
+          </h2>
+          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500  sm:text-xl">
+            Got a medical issue and dont know what do do about it? Need details
+            about your next consultation? Let us know.
+          </p>
+          <form action="#" className="space-y-8">
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900">
+                Your email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 "
+                placeholder="johndoe@gmail.com"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="subject"
+                className="block mb-2 text-sm font-medium text-gray-900 ">
+                Subject
+              </label>
+              <input
+                type="text"
+                id="subject"
+                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 "
+                placeholder="Let us know how we can help you"
+                required
+              />
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="message"
+                className="block mb-2 text-sm font-medium text-gray-900 ">
+                Your message
+              </label>
+              <textarea
+                id="message"
+                rows={6}
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
+                placeholder="Leave a comment..."></textarea>
+            </div>
+            <Button type="primary">
+              <span>Send message</span>
+            </Button>
+          </form>
+        </div>
+      </section>
+    </div>
   );
 };
 
