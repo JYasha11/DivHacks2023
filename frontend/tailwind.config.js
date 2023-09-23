b73bf02803fdaceea6940c76dc23699bdb4d20ca
shvam0000 <shvam0000@gmail.com>
2023-09-23 14:42:24 -0400

    💄 custom colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,7 +9,14 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-white': '#DFDFDF',
+        'primary-black': '#3D3D3D',
+        'primary-blue-100': '#47AADF',
+        'primary-blue-200': '#3A84F3',
+      },
+    },
   },
   plugins: [],
 };
