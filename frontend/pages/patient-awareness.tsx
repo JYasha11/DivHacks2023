0ec0a902ba9f6fd9671586687b5251702452b403
shvam0000 <shvam0000@gmail.com>
2023-09-24 11:21:58 -0400

    ✨ add chatbot feature

---
+++ frontend/pages/patient-awareness.tsx
@@ -0,0 +1,12 @@
+import { Hero } from '@/components/patient-awareness';
+import React from 'react';
+
+const PatientAwareness = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+};
+
+export default PatientAwareness;
